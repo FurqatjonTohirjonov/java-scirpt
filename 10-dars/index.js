c2415d78596848e13b0e7c4e11c8e2f696e46132
Furqatjon2012 <furqatjon200@gmail.com>
2025-04-24 18:34:37 +0300

    java-script

---
+++ 10-dars/index.js
@@ -0,0 +1,53 @@
+//let user = {
+//    id: 1,
+//    name: "Eshmat",
+//    age: 65,
+//    status: "Nafaqat oluvchi",
+//    child: {
+//        id: 2,
+//        name: "Toshmat",
+//        age: 30,
+//        status: "Talaba"
+//    }
+//}
+
+//let clonUser = structuredClone(user);
+
+//clonUser.child.age = 45
+
+//console.log(user);
+
+//console.log(clonUser);
+
+//=================================================
+
+//let user = {
+//   id: 1,
+//    name: "Eshmat",
+//    age: 65,
+//    status: "Nafaqat oluvchi",
+//    child: {
+//        id: 2,
+//        name: "Toshmat",
+//        age: 30,
+//        status: "Talaba",
+//        func() {
+//            return this.age;
+//        }
+//    }
+//};
+
+//let clonUser = {};
+
+//clonUser = {
+//    ...user,
+//    child: {
+//        ...user.child,
+//        func: user.child.func
+//    }
+//};
+
+//clonUser.child.age = 45;
+
+//console.log(user);
+//console.log(clonUser);
