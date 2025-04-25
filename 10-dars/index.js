ff7b5aa37e8858ae77e065692992e80689a53c2a
Furqatjon2012 <furqatjon200@gmail.com>
2025-04-25 11:27:30 +0300

    java-script

--- 10-dars/index.js
+++ 10-dars/index.js
@@ -19,6 +19,8 @@
 
 //console.log(clonUser);
 
+//============================================================================================
+
 //let user = {
 //   id: 1,
 //    name: "Eshmat",
@@ -35,17 +37,22 @@
 //    }
 //};
 
-//let clonUser = {};
-
-//clonUser = {
-//    ...user,
+//let clonUser = {
+//    id: 1,
+//    name: "Eshmat",
+//    age: 65,
+//    status: "Nafaqat oluvchi",
 //    child: {
-//        ...user.child,
-//        func: user.child.func
+//        id: 2,
+//        name: "Toshmat",
+//        age: 45,
+//        status: "Talaba",
+//        func() {
+//            return this.age;
+//        }
 //    }
-//};
-
-//clonUser.child.age = 45;
+//}
 
 //console.log(user);
+
 //console.log(clonUser);
